0b1db66a502b1d4c26773b110e7fa4043f08392a
amujika <amujika009@ikasle.ehu.es>
2014-11-25 12:29:55 +0100

    Main structure created

---
+++ src/agents/Host.java
@@ -0,0 +1,12 @@
+package agents;
+
+import jade.core.Agent;
+
+public class Host extends Agent {	
+
+	private static final long serialVersionUID = 3493143872944535412L;
+
+	protected void setup() {
+		System.out.println("Everything is working");
+	}
+}
